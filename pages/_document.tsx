222bc5041e802408a5349450e669716fcd083825
Patrick Santos <patrickspteixeira@gmail.com>
2022-03-27 14:29:42 -0300

    feat: add chakra-ui

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import { ColorModeScript } from '@chakra-ui/react'
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
+import { theme } from './config/theme'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+      return (
+        <Html lang='en'>
+            <Head />
+            <body>
+                <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+      )
+  }
+}
+
+export default MyDocument
