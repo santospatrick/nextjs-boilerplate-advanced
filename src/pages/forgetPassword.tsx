fccab0f22f26e4e1f895fd42c1c824f4cc332eab
Lincoln Costa <lincoln.mcosta@gmail.com>
2022-06-07 21:32:46 -0300

    feat: forget password page

--- src/pages/forgetPassword.tsx
+++ src/pages/forgetPassword.tsx
@@ -1,20 +1,31 @@
 import ForgetPasswordForm, {
   FormValues,
 } from "@/components/forms/ForgetPasswordForm";
-import { Box, Container } from "@chakra-ui/react";
+import api from "@/services/api";
+import { Box, Container, Text } from "@chakra-ui/react";
+import { useState } from "react";
 import { SubmitHandler } from "react-hook-form";
 import { toast } from "react-toastify";
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 function ForgetPassword() {
-  const onSubmit: SubmitHandler<FormValues> = async (_values) => {
-    await sleep(1000);
-    toast.success(
-      "Nice submission! take a look at the values on your browser console."
-    );
+  const [sent, setSent] = useState(false);
+  const onSubmit: SubmitHandler<FormValues> = async (values) => {
+    setSent(true);
+    await api.post("/auth/reset-password", {
+      email: values.email,
+    });
+    toast.success("E-mail sent successfully.");
   };
 
+  if (sent) {
+    return (
+      <Container maxW="container.sm">
+        <Box py={10}>
+          <Text>Verify your e-mail and follow the instructions.</Text>
+        </Box>
+      </Container>
+    );
+  }
   return (
     <Container maxW="container.sm">
       <Box py={10}>
